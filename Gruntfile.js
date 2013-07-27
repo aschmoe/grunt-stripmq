45e1e44273745fc691fde6c8c6d5160e4395c629
Alex Schmoe <aschmoe@gmail.com>
2013-07-26 18:08:29 -0700

    updated to new approach.  globbing, min max values, base css stripping

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,48 @@
 'use strict';
 
 module.exports = function (grunt) {
-    // Project configuration.
-    grunt.initConfig({
-        jshint: {
-            all: [
-                'Gruntfile.js',
-                'tasks/**/*.js'
-            ],
-            options: {
-                jshintrc: '.jshintrc'
-            }
-        },
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/**/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
 
-        // Configuration to be run (and then tested).
-        stripmq: {
-            all: {
-                files: {
-                    'test/output.css': ['test/input.css']
-                }
-            }
-        }
-    });
+    // Configuration to be run (and then tested).
+    stripmq: {
+      options: {
+        stripBase: true,
+        minWidth: 600,
+        maxWidth: 800
+      },
+      all: {
+        /*files: [
+            {src: ['test/input/*.css'], dest: 'test/output/single.css'}
+        ]*/
+        files: [
+          {
+            expand: true,
+            cwd: 'test/input',
+            src: ['*.css'],
+            dest: 'test/output/',
+            ext: '-ie.css',
+          },
+        ]
+      }
+    }
+  });
 
-    // Actually load this plugin's task(s).
-    grunt.loadTasks('tasks');
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
 
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
-    // By default, lint and run all tests.
-    grunt.registerTask('default', ['jshint', 'stripmq']);
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'stripmq']);
 };
--- tasks/lib/stripmq.js
+++ tasks/lib/stripmq.js
@@ -1,18 +1,26 @@
 'use strict';
 
 var fs = require('fs'),
-    _ = require('underscore'),
-    Parser = require('css-parse'),
-    Stringify = require('css-stringify/lib/compress');
+  _ = require('underscore'),
+  Parser = require('css-parse'),
+  Stringify = require('css-stringify/lib/compress');
+
+Stringify.prototype.compile = function(node){
+  if(this.viewport.stripBase) {
+  node.stylesheet.rules = _.where(node.stylesheet.rules, {type: "media"});
+  }
+  return node.stylesheet
+  .rules.map(this.visit, this)
+  .join('');
+};
 
 
 // overwrite media compiler method
 Stringify.prototype.media = function(node) {
-    if(!this.matchMedia(node.media)) {
-        return '';
-    }
-
-    return node.rules.map(this.visit, this).join('');
+  if(!this.matchMedia(node.media)) {
+    return '';
+  }
+  return node.rules.map(this.visit, this).join('');
 };
 
 
@@ -22,58 +30,61 @@ Stringify.prototype.media = function(node) {
  * @returns {boolean}
  */
 Stringify.prototype.matchMedia = function(str) {
-    var self = this,
-        queries = str.toLowerCase().match(/\((.+)\)/gi),
-        matches = [];
+  var self = this,
+    queries = str.toLowerCase().match(/\((.+)\)/gi),
+    matches = [];
 
-    queries.forEach(function(query) {
-        // min/max is should be the first property, then the name of the property (like width)
-        var keyval = query.replace(/\(|\)/g, "").split(":"),
+  queries.forEach(function(query) {
+    // min/max is should be the first property, then the name of the property (like width)
+    var keyval = query.replace(/\(|\)/g, "").split(":"),
 
-            // property parts
-            property = keyval[0].trim().match(/(min|max)-(.+)$/),
+      // property parts
+      property = keyval[0].trim().match(/(min|max)-(.+)$/),
 
-            prop_value, setting_value;
+      prop_value, setting_value_min, setting_value_max;
 
-        // no min/max property found
-        if(!property) {
-            return;
-        }
+    // no min/max property found
+    if(!property) {
+      return;
+    }
 
-        // find setting values by properties
-        if(keyval[0].match(/width/i)) {
-            setting_value = self.viewport.width;
-        }
+    // find setting values by properties
+    if(keyval[0].match(/width/i)) {
+      setting_value_min = self.viewport.minWidth;
+      setting_value_max = self.viewport.maxWidth;
+    }
 
-        else if(keyval[0].match(/height/i)) {
-            setting_value = self.viewport.height;
-        }
+    else if(keyval[0].match(/height/i)) {
+      setting_value_min = self.viewport.minHeight;
+      setting_value_max = self.viewport.maxHeight;
+    }
 
-        else if(keyval[0].match(/device-pixel-ratio/i)) {
-            setting_value = self.viewport["device-pixel-ratio"];
+    else if(keyval[0].match(/device-pixel-ratio/i)) {
+      setting_value_min = self.viewport["min-device-pixel-ratio"];
+      setting_value_max = self.viewport["max-device-pixel-ratio"];
 
-            // opera device pixel ratio is different, uses 3/2 for 1.5, kind of weird
-            if(keyval[1].match(/[0-9]+\s*\/\s*[0-9]+/)) {
-                var values = keyval[1].split("/");
-                prop_value = parseInt(values[0],10) / parseInt(values[1],10);
-            }
-        }
+      // opera device pixel ratio is different, uses 3/2 for 1.5, kind of weird
+      if(keyval[1].match(/[0-9]+\s*\/\s*[0-9]+/)) {
+        var values = keyval[1].split("/");
+        prop_value = parseInt(values[0],10) / parseInt(values[1],10);
+      }
+    }
 
-        // parse the value of the property
-        prop_value = parseFloat(prop_value || keyval[1]);
+    // parse the value of the property
+    prop_value = parseFloat(prop_value || keyval[1]);
 
-        switch(property[1]) {
-            case 'min':
-                matches.push(setting_value >= prop_value);
-                break;
+    switch(property[1]) {
+      case 'min':
+        matches.push((setting_value_min <= prop_value) && (setting_value_max >= prop_value));
+        break;
 
-            case 'max':
-                matches.push(setting_value <= prop_value);
-                break;
-        }
-    });
+      case 'max':
+        matches.push((setting_value_max >= prop_value) && (setting_value_min <= prop_value));
+        break;
+    }
+  });
 
-    return matches.indexOf(false) === -1;
+  return matches.indexOf(false) === -1;
 };
 
 
@@ -82,9 +93,13 @@ Stringify.prototype.matchMedia = function(str) {
  * @type {{device-pixel-ratio: number, width: number, height: number}}
  */
 Stringify.prototype.viewport = {
-    "device-pixel-ratio": 1,
-    "width": 1024,
-    "height": 768
+  "stripBase": false,
+  "min-device-pixel-ratio": 1,
+  "max-device-pixel-ratio": 2,
+  "minWidth": 0,
+  "maxWidth": 1024,
+  "minHeight": 0,
+  "maxHeight": 768
 };
 
 
@@ -94,14 +109,15 @@ Stringify.prototype.viewport = {
  * @returns {string} output
  */
 function StripMQ(input, options) {
-    var tree = new Parser(input);
-    var compiler = new Stringify({});
+  //parseOptions = options.stripBase ? {}
+  var tree = new Parser(input);
+  var compiler = new Stringify({});
 
-    if(options) {
-        _.extend(compiler.viewport, options);
-    }
+  if(options) {
+    _.extend(compiler.viewport, options);
+  }
 
-    return compiler.compile(tree);
+  return compiler.compile(tree);
 }
 
 module.exports = StripMQ;
